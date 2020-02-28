8bca3b973d5dfb51699dc1a6258e3195c2b6dda3
MuroriM <donmurori@gmail.com>
2020-02-27 21:27:17 -0500

    Push 2: Made modifications to allow fetch request

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,9 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function getGreetingAsyncAwait() {
+  const response = await fetch('/data');
+  const quote = await response.text();
+  document.getElementById('greeting-container').innerText = quote;
+}
